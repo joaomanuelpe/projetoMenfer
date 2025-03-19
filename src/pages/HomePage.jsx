b74bd021f891876a2e02c69ffa447b488eebd06f
arcesti <arcesti15@gmail.com>
2025-03-19 16:26:31 -0300

    Alter user

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ import { useAuth } from "../context/AuthContext";
 const HomePage = () => {
   const [verse, setVerse] = useState("");
   const [notifications, setNotifications] = useState(0);
-  const { user }= useAuth();
+  const { user } = useAuth();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -14,10 +14,10 @@ const HomePage = () => {
       try {
         const response = await fetch(
           "https://www.abibliadigital.com.br/api/verses/nvi/random", {
-            headers: {
-              'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdHIiOiJXZWQgTWFyIDE5IDIwMjUgMTc6MzA6NDAgR01UKzAwMDAuYXJjZXN0aTE1QGdtYWlsLmNvbSIsImlhdCI6MTc0MjQwNTQ0MH0.4ebn3oh81wI0DEJud_Czm0KagyOYTd2TOHV_ArSxfX4'
-            },
-          }
+          headers: {
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdHIiOiJXZWQgTWFyIDE5IDIwMjUgMTc6MzA6NDAgR01UKzAwMDAuYXJjZXN0aTE1QGdtYWlsLmNvbSIsImlhdCI6MTc0MjQwNTQ0MH0.4ebn3oh81wI0DEJud_Czm0KagyOYTd2TOHV_ArSxfX4'
+          },
+        }
         );
         const data = await response.json();
         setVerse(
@@ -37,11 +37,38 @@ const HomePage = () => {
     <div className="min-h-screen bg-blue-50">
       <header className="bg-blue-600 text-white py-4 px-6 shadow-lg">
         <div className="max-w-6xl mx-auto flex justify-between items-center">
-          <h1 className="text-2xl font-bold">Bem-vindo!</h1>
+          <h1 className="text-2xl font-bold">Menferlog</h1>
           <div className="flex items-center space-x-4">
-            <span className="flex items-center">
-              <Users className="w-5 h-5 mr-2" />
-              {user ? user.username : "Usuario Default"}
+            <span className="flex items-center text-xl gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                width="40"
+                height="40"
+                style={{ enableBackground: "new 0 0 512 512" }} // Correção do estilo
+              >
+                <g id="_x31_64_x2CAdd_x2CContact_x2CUser_x2CTwitter">
+                  <g>
+                    <path
+                      d="M475.02,383.299v13.199H301v-13.199c0-39.58-17.42-75.08-45-99.27c23.23-20.381,53.68-32.741,87.01-32.741C415.92,251.289,475.02,310.389,475.02,383.299z"
+                      fill="#40B3E2"
+                    />
+                  </g>
+                  <g>
+                    <path
+                      d="M256,284.029c27.58,24.189,45,59.689,45,99.27v13.199h-90H36.98v-13.199c0-36.461,14.78-69.461,38.66-93.35c23.89-23.891,56.9-38.661,93.35-38.661C202.32,251.289,232.77,263.648,256,284.029z"
+                      fill="#86DCFD"
+                    />
+                  </g>
+                  <g>
+                    <circle cx="343.01" cy="181.979" r="68.48" fill="#EABD8C" />
+                  </g>
+                  <g>
+                    <circle cx="168.99" cy="181.979" r="68.48" fill="#FFD8A8" />
+                  </g>
+                </g>
+              </svg>
+              {user ? user.username : "Usuário Default"}
             </span>
           </div>
         </div>
